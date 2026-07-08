3c713ccf5f7ce9005d701a15c4071ddb6ed6179d
cracal <cracal@163.com>
2026-07-08 22:42:41 +0800

    refactor(assert): 修改assert

--- src/cra_assert.c
+++ src/cra_assert.c
@@ -4,18 +4,13 @@ static void
 cra_assert_default_func(const char *condition, const char *fname, const char *file, int line)
 {
     fprintf(stderr, "assert failed: `%s`, %s:%d:%s\n", condition, file, line, fname);
+    fflush(stderr);
+
 #if defined(CRA_COMPILER_GNUC)
     __builtin_trap();
 #elif defined(CRA_COMPILER_MSVC)
     __debugbreak();
 #endif
-    exit(1);
 }
 
-void (*__cra_assert_func)(const char *cond, const char *fname, const char *file, int line) = cra_assert_default_func;
-
-void
-cra_assert_set_func(void (*func)(const char *condition, const char *fname, const char *file, int line))
-{
-    __cra_assert_func = func;
-}
+void (*__cra_g_assert__)(const char *cond, const char *fname, const char *file, int line) = cra_assert_default_func;
